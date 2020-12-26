c7bbf46b8d903138d55ef919fd7d379542bdc328
Your Name <hayri.durmaz@tedu.edu.tr>
2020-12-26 18:05:15 +0300

    dataset added, training added

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+import binascii
 import os
 
 import cv2
 import numpy as np
+import scipy
+import scipy.misc
+import scipy.cluster
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
-def color_histogram_of_image(img, isTraining, label):
+def color_histogram_of_image(img, isTraining, label, mask=None):
     features = []
     counter = 0
     channels = cv2.split(img)
     colors = ('b', 'g', 'r')
     feature_data = ''
     for (i, col) in zip(channels, colors):  # Loop over the image channels
-        hist = cv2.calcHist([i], [0], None, [256], [0, 256])  # Create a histogram for current channel
+        histogramUpperLimit = 250
+        histogramLowerLimit = 5
+        if label == "White":
+            histogramUpperLimit = 256
+        if label == "Black":
+            histogramLowerLimit = 0
+        hist = cv2.calcHist([i], [0], mask, [256],
+                            [histogramLowerLimit, histogramUpperLimit])  # Create a histogram for current channel
         features.extend(hist)
         elem = np.argmax(hist)  # find the peak pixel values for R, G, and B
         if counter == 0:
@@ -29,15 +42,48 @@ def color_histogram_of_image(img, isTraining, label):
         print("else")
 
 
+def training():
+    # red color training images
+    for color in os.listdir('./training_dataset/colors'):
+        path = "./training_dataset/colors/" + color + "/"
+        for file in os.listdir('./training_dataset/colors/' + color):
+            img = cv2.imread(path + file)  # Load the image
+            color_histogram_of_image(img, True, color)
+
+
+def findDominantColor(im):
+    NUM_CLUSTERS = 5
+
+    print('reading image')
+    # im = im.resize((150, 150))  # optional, to reduce time
+    ar = np.asarray(im)
+    shape = ar.shape
+    ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
+
+    print('finding clusters')
+    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+    print('cluster centres:\n', codes)
+
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)  # assign codes
+    counts, bins = np.histogram(vecs, len(codes))  # count occurrences
+
+    index_max = np.argmax(counts)  # find most frequent
+    peak = codes[index_max]
+    colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+    print('most frequent is %s (#%s)' % (peak, colour))
+
+
 if __name__ == '__main__':
     # data = pd.read_csv("data/final_data.csv")
     # print(data)
 
-    images = []
-    path = "images/"
-    for image in os.listdir(path):
-        images.append(image)
+    # images = []
+    # path = "images/"
+    # for image in os.listdir(path):
+    #     images.append(image)
+    #
+    # for image in images:
+    #     img = cv2.imread("%s%s" % (path, image))  # Load the image
+    #     color_histogram_of_image(img, True, "black")
 
-    for image in images:
-        img = cv2.imread("%s%s" % (path, image))  # Load the image
-        color_histogram_of_image(img, True, "black")
+    training()
